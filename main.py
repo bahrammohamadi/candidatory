b930d2f44f39b885e35705c81ded3cfe0e6b8943
bahrammohamadi <lasvaram@gmail.com>
2026-02-18 11:28:44 +0330

    Update channel news text in final output

--- main.py
+++ main.py
@@ -92,10 +92,10 @@ async def main(event=None, context=None):
                     f"{title}\n\n"
                     f"@candidatoryiran\n\n"
                     f"{description}\n\n"
-                    f"*** کانال خبری کاندیداتوری ***\n"
+                    f"🆔 کانال خبری کاندیداتوری \n"
                     f"🆔 @candidatoryiran\n"
                     f"🆔 Instagram.com/candidatory.ir"
-                    f"  \n\n"
+                    f".  \n\n"
                 )
 
                 image_url = None
